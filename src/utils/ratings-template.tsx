8f83cf89c20336a3f0c0304eb3bbcab2e2a549b5
Didier Hartong <info@focuswider.com>
2023-01-27 15:22:12 +0100

    fix(RatingsTemplate): make uuid uppercased for relation is null error

--- src/utils/ratings-template.tsx
+++ src/utils/ratings-template.tsx
@@ -16,7 +16,7 @@ export function getRatingsTemplate(visitatieId: number): VisitatieBeoordelingCat
     weighing: number,
   ): VisitatieBeoordelingCategorieVraagInput => {
     return {
-      VisitatieBeoordelingCategorieVraagID: uuidv4(),
+      VisitatieBeoordelingCategorieVraagID: uuidv4().toUpperCase(),
       VisitatieBeoordelingCategorieID: categorieId,
       CategorieTemplateID: categoryTemplateId,
       VraagTemplateID: questionTemplateId,
@@ -42,7 +42,7 @@ export function getRatingsTemplate(visitatieId: number): VisitatieBeoordelingCat
   ): VisitatieBeoordelingCategorieInput => {
     return {
       VisitatieID: visitatieId,
-      VisitatieBeoordelingCategorieID: uuidv4(),
+      VisitatieBeoordelingCategorieID: uuidv4().toUpperCase(),
       CategorieTemplateID: categoryId,
       CategorieNaam: name,
       Weging: weighing,
@@ -64,27 +64,39 @@ export function getRatingsTemplate(visitatieId: number): VisitatieBeoordelingCat
 
   return [
     newCategory(catId1, 'Uitvoering van doel en inhoud', 50, [
-      newQuestion(catId1, uuidv4(), 1001, 'Beginsituatie deelnemers nagegaan', 5),
-      newQuestion(catId1, uuidv4(), 1002, 'Op- en aanmerkingen beoordelaar verwerkt', 7),
-      newQuestion(catId1, uuidv4(), 1003, 'Doelstelling behaald', 13),
+      newQuestion(catId1, uuidv4().toUpperCase(), 1001, 'Beginsituatie deelnemers nagegaan', 5),
       newQuestion(
         catId1,
-        uuidv4(),
+        uuidv4().toUpperCase(),
+        1002,
+        'Op- en aanmerkingen beoordelaar verwerkt',
+        7,
+      ),
+      newQuestion(catId1, uuidv4().toUpperCase(), 1003, 'Doelstelling behaald', 13),
+      newQuestion(
+        catId1,
+        uuidv4().toUpperCase(),
         1004,
         'Voorgenomen inhoud behandeld, event. incl thuisopdracht',
         25,
       ),
     ]),
     newCategory(catId2, 'Aanpak', 30, [
-      newQuestion(catId2, uuidv4(), 2001, 'Werkwijze als gepland toegepast', 10),
-      newQuestion(catId2, uuidv4(), 2002, 'Deelnemers actief betrokken', 10),
-      newQuestion(catId2, uuidv4(), 2003, 'Deelnemers delen kennis en info', 10),
+      newQuestion(catId2, uuidv4().toUpperCase(), 2001, 'Werkwijze als gepland toegepast', 10),
+      newQuestion(catId2, uuidv4().toUpperCase(), 2002, 'Deelnemers actief betrokken', 10),
+      newQuestion(catId2, uuidv4().toUpperCase(), 2003, 'Deelnemers delen kennis en info', 10),
     ]),
     newCategory(catId3, 'Organisatie', 20, [
-      newQuestion(catId3, uuidv4(), 3001, 'Samenvatting uitgereikt', 5),
-      newQuestion(catId3, uuidv4(), 3002, 'Inhoudelijk en organisatorisch geëvalueerd', 5),
-      newQuestion(catId3, uuidv4(), 3003, 'Groepsgrootte als gepland', 5),
-      newQuestion(catId3, uuidv4(), 3004, 'Localiteit en (digi)techniek adequaat', 5),
+      newQuestion(catId3, uuidv4().toUpperCase(), 3001, 'Samenvatting uitgereikt', 5),
+      newQuestion(
+        catId3,
+        uuidv4().toUpperCase(),
+        3002,
+        'Inhoudelijk en organisatorisch geëvalueerd',
+        5,
+      ),
+      newQuestion(catId3, uuidv4().toUpperCase(), 3003, 'Groepsgrootte als gepland', 5),
+      newQuestion(catId3, uuidv4().toUpperCase(), 3004, 'Localiteit en (digi)techniek adequaat', 5),
     ]),
   ];
 }
